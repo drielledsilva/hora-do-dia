45e8167cca680258c45a5afdaa928a0375548d01
Drielle da Silva <drielle.silva@live.com>
2022-01-24 19:36:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function carregar() {
-    let msg = window.document.getElementById('msg')
-    let img = window.document.getElementById('imagem')
-    let data = new Date()
-    let hora = data.getHours()
-    let minutos = data.getMinutes()
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
